4685acb509967859b0c30dd557a78d2758d0b116
Erdogan <yildizcontact@gmail.com>
2023-11-20 19:37:59 +0300

    RAG Tests on uni-data.

--- rag/rag.py
+++ rag/rag.py
@@ -5,7 +5,7 @@ from llm.contexts.mistral_context import MistralContext
 from llm.mistral_llm import MistralLLM
 from utils.enums import MistralTypes
 from utils.protocols import Doc
-
+import os
 
 class DefaultRetrievalAugmentedGenerator:
     def __init__(self, docs:List[Doc]):
@@ -14,7 +14,7 @@ class DefaultRetrievalAugmentedGenerator:
         self._kb = DefaultKnowledgeBase(docs=docs, embedder_name="all-mpnet-base-v2")
 
     def ask(self, question:str):
-        related_docs = self._kb.search(q=question, k=5)
+        related_docs = self._kb.search(q=question, k=3)
         #concatenate related docs accordingly obtain the required llm context
         context = self.__prepare_retrieved_content(related_docs=related_docs, question=question)
         #ask question to the llm utilizing the retrieved context
@@ -26,11 +26,11 @@ class DefaultRetrievalAugmentedGenerator:
         for i,doc in enumerate(related_docs):
             source = doc.metadata["source"]
             content = doc.page_content
-            retrieved_content += f"Source{i}:{source}\nContent{i}:{content}\n\n-----------\n"
+            retrieved_content += f"Source{i}:{os.path.basename(source)}\nContent{i}:{content}\n\n-----------\n" # f"Content{i}:{content}\n-----------\n" #
 
         context = MistralContext()
-        context.add_user_message(entry=f"Please answer the following user question utilizing the content provided below. If the content is not sufficient to answer this question, please"
-                                       f"answer, 'Sorry, I could not find the answer in my knowledge base.'\n\nHere is the question:{question}\n\nHere is the content:{retrieved_content}.")
+        context.add_user_message(entry=f"Please answer the following user question utilizing the content(s) provided below and specify names of source pdfs that you have used for the answer. If the content(s) is not sufficient to answer this question, please "
+                                       f"ONLY answer, 'Sorry, I could not find the answer in my knowledge base.'\n\nHere is the question:{question}\n\nHere is the content(s):\n{retrieved_content}.")
         return context
 
 
--- rag_test.py
+++ rag_test.py
@@ -1,12 +1,15 @@
 from rag.rag import DefaultRetrievalAugmentedGenerator
 from splitters.pdf_splitter import PdfSplitter
+import time
 
-pdf_splitter = PdfSplitter(local_src_path="./storage/sources/tsps")
+pdf_splitter = PdfSplitter(chunk_size=750, chunk_overlap=0, local_src_path="./storage/sources/uni")
 splits = pdf_splitter.split()
 
 test_rag = DefaultRetrievalAugmentedGenerator(docs=splits)
 while True:
     q = input("Enter your question:")
+    st = time.time()
     answer = test_rag.ask(question=q)
     print(answer)
-    print("--------------------------")
+    et = time.time()
+    print(f"-------- {et-st:.2f} seconds ----------")
