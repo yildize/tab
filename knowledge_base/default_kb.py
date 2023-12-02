3a106dd03ea83f188f75b13d94cf6c49e3b19b6b
Erdogan <yildizcontact@gmail.com>
2023-12-02 20:43:20 +0300

    Add cross-encoder functionality to default kb. Not tested.

--- knowledge_base/default_kb.py
+++ knowledge_base/default_kb.py
@@ -1,7 +1,9 @@
+import os
 from knowledge_base.knowledge_base import CustomKnowledgeBase
-from typing import Union, List
+from typing import Union, List, Optional
 from utils.protocols import Doc
 import faiss
+from numpy import np
 from knowledge_base.embedders import SetenceTransformerEmbedder
 
 
@@ -9,8 +11,8 @@ class DefaultKnowledgeBase(CustomKnowledgeBase):
     db: faiss.IndexFlatIP
     embedder: SetenceTransformerEmbedder
 
-    def __init__(self, docs: Union[str, List[Doc]], embedder_name:str):
-        super().__init__(docs=docs, embedder=SetenceTransformerEmbedder(embedder_name=embedder_name))
+    def __init__(self, docs: Union[str, List[Doc]], embedder_name:str, cross_encoder_name: Optional[str]=None):
+        super().__init__(docs=docs, embedder=SetenceTransformerEmbedder(embedder_name=embedder_name), cross_encoder_name=cross_encoder_name)
 
     def _construct_storage(self):
         embeddings = self.embedder([doc.page_content for doc in self.docs])
@@ -19,7 +21,7 @@ class DefaultKnowledgeBase(CustomKnowledgeBase):
         db.add(embeddings)
         return db
 
-    def search(self, q, k=3):
+    def search(self, q, k=3) -> List[Doc]:
         query_embedding = self.embedder(q)
         faiss.normalize_L2(query_embedding)
         distances, indices = self.db.search(x=query_embedding, k=k)
@@ -30,3 +32,31 @@ class DefaultKnowledgeBase(CustomKnowledgeBase):
             similar_doc.metadata["retrieval_info"] = {"q": q, "dist": dist, "token_len": self.embedder.len_required_tokens(similar_doc.page_content)}
             res_docs.append(similar_doc)
         return res_docs
+
+    def search_pages_with_cross_encoder(self, q, max_k=5, cross_encoder_input_k=20) -> List[Doc]:
+        """ Note that for this search method, the number of the return documents can be lower than
+        max_k when multiple chunks are from the same page. Also note that this method relies on
+        docs having doc_index field inside their metadata."""
+        # First retrieve similar chunks by regular kb search
+        similar_docs = self.search(q=q, k=cross_encoder_input_k) # each doc is typically a chunk.
+        # Now pass them to cross_encoder for finer similarity analysis. It will compare question to similar doc contents:
+        scores = self.cross_encoder.predict([[q, doc.page_content] for doc in similar_docs])
+
+        # Now only top k most related docs will be selected
+        sorted_indices = np.argsort(-scores) # sort in a decreasing score order.
+        docs_indexes = {} # stores the doc indexes of most similar docs and their scores.
+        for i in sorted_indices:
+            doc = similar_docs[i]
+            if doc.metadata.get('doc_index') is None: raise AttributeError('search_pages_with_cross_encoder methods needs doc_index attribute for the docs in order to perform!')
+            docs_indexes[doc.metadata["doc_index"]] = scores[i]
+            if len(docs_indexes) >= max_k: break
+
+        # Now extract and deep copy those most similar page-docs to thread-safely modify them:
+        related_page_docs = []
+        for doc_index, score in docs_indexes.items():
+            doc = self.docs[doc_index].copy(deep=True)
+            doc.metadata["retrieval_info"] = {"q": q, "dist": score} # I kept the key as dist even though it is not exactly a distance measure just too keep it compatible.
+            related_page_docs.append(doc)
+
+        return related_page_docs
+
--- knowledge_base/knowledge_base.py
+++ knowledge_base/knowledge_base.py
@@ -1,17 +1,26 @@
 
 from abc import ABC, abstractmethod
+
+from sentence_transformers import CrossEncoder
+
 from knowledge_base.embedders import Embedder
 from typing import Union, List
 from utils.protocols import Doc
 from utils.utils import load_docs
-
+from typing import Optional
+import os
+from utils.utils import ROOT_PATH
 
 class CustomKnowledgeBase(ABC):
 
-    def __init__(self, docs:Union[str, List[Doc]], embedder:Embedder):
+    def __init__(self, docs:Union[str, List[Doc]], embedder:Embedder, cross_encoder_name: Optional[str]):
         # If a path is specified as docs let's load it:
         self.docs = load_docs(docs) if isinstance(docs, str) else docs
         self.embedder = embedder
+        if cross_encoder_name is not None:
+            cache_dir = os.path.join(ROOT_PATH, "storage", "cross-encoders")
+            self._cross_encoder = CrossEncoder(model_name=cross_encoder_name, tokenizer_args={'cache_dir': cache_dir}, automodel_args={'cache_dir': cache_dir},  max_length=512)
+
         self._check_chunk_length()
 
         self.db = self._construct_storage()
@@ -28,3 +37,9 @@ class CustomKnowledgeBase(ABC):
     @abstractmethod
     def search(self, *args, **kwargs) -> List[Doc]:
         ...
+
+    @property
+    def cross_encoder(self):
+        if hasattr(self, "_cross_encoder"): return self._cross_encoder
+        raise AttributeError("You are trying to reach the cross_encoder attribute of the knowledge base but you haven't"
+                             "provided a cross_encoder_name to constructor.")
